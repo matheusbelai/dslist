f82ef1ab09975c5a8e84c65072820c32dd0a0c9e
matheusbelai <matheusbelai01@gmail.com>
2023-07-16 12:43:46 -0300

    consulta sql, busca de games por lista e projection

---
+++ src/main/java/com/belai/dslist/projection/GameMinProjection.java
@@ -0,0 +1,16 @@
+package com.belai.dslist.projection;
+
+public interface GameMinProjection {
+
+    Long getId();
+
+    String getTitle();
+
+    Integer getYear();
+
+    String getImgUrl();
+
+    String getShortDescription();
+
+    Integer getPosition();
+}
--- src/main/java/com/belai/dslist/service/GameService.java
+++ src/main/java/com/belai/dslist/service/GameService.java
@@ -5,6 +5,7 @@ import com.belai.dslist.dto.GameDTO;
 import com.belai.dslist.dto.GameListDTO;
 import com.belai.dslist.dto.GameMinDTO;
 import com.belai.dslist.entities.Game;
+import com.belai.dslist.projection.GameMinProjection;
 import com.belai.dslist.repository.GameRespository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -30,7 +31,11 @@ public class GameService {
          return new GameDTO(result);
     }
 
-
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> result = gameRespository.searchByList(listId);
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
 
 
 }
